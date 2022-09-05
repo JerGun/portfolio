519f5aa5a1d0173602f2deae05efbd160cfd1886
pawaret <62021393@up.ac.th>
2022-09-06 03:34:02 +0700

    Hotfix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production"
-const path = require("path")
 
 const nextConfig = {
-  // target: "serverless",
-  exportPathMap: () => ({
-    "/": {
-      page: "/",
-    },
-  }),
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
   experimental: {
     scrollRestoration: true,
   },
@@ -20,8 +10,7 @@ const nextConfig = {
     path: "",
   },
   reactStrictMode: true,
-  assetPrefix: isProd ? process.env.ASSET_PREFIX : "",
-  swcMinify: true,
+  assetPrefix: isProd ? "/portfolio-x" : "",
   env: {
     INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID,
     INFURA_API_KEY: process.env.INFURA_API_KEY,
