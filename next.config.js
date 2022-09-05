8540dcdde22e8e5b24b1d26e155a080ff5ae3c75
pawaret <62021393@up.ac.th>
2022-09-06 03:55:27 +0700

    Update CI/CD

--- next.config.js
+++ next.config.js
@@ -10,7 +10,9 @@ const nextConfig = {
     path: "",
   },
   reactStrictMode: true,
-  assetPrefix: isProd ? "/portfolio-x" : "",
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/JerGun/jergun.github.io/gh-pages/"
+    : "",
   env: {
     INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID,
     INFURA_API_KEY: process.env.INFURA_API_KEY,
