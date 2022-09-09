96a7be3e9e838bbbdcb1997a0ebaab2c5b1ba8ef
pawaret <62021393@up.ac.th>
2022-09-10 04:46:04 +0700

    config

--- next.config.js
+++ next.config.js
@@ -10,10 +10,10 @@ const nextConfig = {
     path: "",
   },
   reactStrictMode: true,
-  basePath: isProd ? "" : "/portfolio-x",
-  assetPrefix: isProd
-    ? ""
-    : "https://cdn.statically.io/gh/JerGun/portfolio-x/gh-pages/",
+  // basePath: isProd ? "" : "/portfolio-x",
+  // assetPrefix: isProd
+  //   ? ""
+  //   : "https://cdn.statically.io/gh/JerGun/portfolio-x/gh-pages/",
   env: {
     INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID,
     INFURA_API_KEY: process.env.INFURA_API_KEY,
