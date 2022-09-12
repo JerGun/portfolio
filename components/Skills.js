5dbcfc89e8295c3dc6d1c347adc3efdfd38de561
pawaret <62021393@up.ac.th>
2022-09-13 01:41:27 +0700

    Add components

---
+++ components/Skills.js
@@ -0,0 +1,11 @@
+export default function Skills() {
+  return (
+    <div className="h-screen w-full bg-black">
+      <p>asdadsadads</p>
+      <p>asdadsadads</p>
+      <p>asdadsadads</p>
+      <p>asdadsadads</p>
+      <p>asdadsadads</p>
+    </div>
+  )
+}
---
+++ components/Work.js
@@ -0,0 +1,11 @@
+export default function Work() {
+    return (
+      <div className="h-screen w-full bg-red-200">
+        <p>asdadsadads</p>
+        <p>asdadsadads</p>
+        <p>asdadsadads</p>
+        <p>asdadsadads</p>
+        <p>asdadsadads</p>
+      </div>
+    )
+  }
