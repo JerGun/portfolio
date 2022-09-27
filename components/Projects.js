3dd7b6ab0c5d63d0f84c5eb697e64727e51e9a6f
Pawaret Muengkaew <pawaret.muengkaew@outlook.com>
2022-09-27 23:34:23 +0700

    Implement project and work components

---
+++ components/Projects.js
@@ -0,0 +1,125 @@
+import {
+  faArrowUpRightFromSquare,
+  faCode,
+} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+export default function Projects({ title, projects, active }) {
+  return (
+    <div className="w-full space-y-10">
+      <div className="space-y-5">
+        <div className="flex space-x-5 items-center">
+          <p className="text-3xl font-bold capitalize">{title}</p>
+          <span className="h-0.5 w-10 bg-white"></span>
+        </div>
+        <div className="flex space-x-1">
+          <p>
+            All small gallery of recent projects chosen by me. Interested to see
+            some more? Visit
+          </p>
+          <a
+            href="https://github.com/JerGun?tab=repositories"
+            target="_blank"
+            className="font-bold text-primary"
+          >
+            my work
+          </a>
+          <p>page.</p>
+        </div>
+      </div>
+      <div className="h-full w-full grid grid-cols-3 gap-10">
+        {projects.map((project, i) => (
+          <div
+            className="h-full rounded-xl flex flex-col justify-between bg-customGrayHeavy transition duration-500 ease-in-out hover:scale-105"
+            key={i}
+          >
+            <div className="h-[300px] w-full rounded-t-xl">
+              <img
+                src={project.img}
+                alt={project.name}
+                className={`h-full w-full rounded-t-xl ${
+                  project.style ? project.style : "object-cover object-center"
+                }`}
+              />
+            </div>
+            <div className="h-fit p-5 space-y-3">
+              <div className="flex items-center justify-between space-x-3">
+                <p className="text-lg font-bold text-primary">{project.name}</p>
+                {/* <span class="relative flex h-3 w-3">
+                  <span
+                    class={`${
+                      project.active ? "bg-green-400" : "bg-red-400"
+                    } animate-ping absolute inline-flex h-full w-full rounded-full opacity-75`}
+                  ></span>
+                  <span
+                    class={`${
+                      project.active ? "bg-green-500" : "bg-red-500"
+                    } relative inline-flex rounded-full h-3 w-3`}
+                  ></span>
+                </span> */}
+                <p className="capitalize text-sm text-customGrayLight">
+                  {project.date}
+                </p>
+              </div>
+              <p>{project.description}</p>
+              <div className="w-full flex flex-col items-center space-y-3">
+                <p className="text-primary">Technologies</p>
+                <div className="w-full flex flex-wrap">
+                  {project.tech.map((tech, i) => (
+                    <div className="flex" key={i}>
+                      <p className={`${i % 2 != 0 && "text-text"}`}>{tech}</p>
+                      <span className="w-5"></span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="flex px-5 pb-5 justify-between">
+              <div className="flex items-center space-x-3">
+                {/* <span class="relative flex h-3 w-3">
+                  <span
+                    class={`${
+                      project.active ? "bg-green-400" : "bg-red-400"
+                    } animate-ping absolute inline-flex h-full w-full rounded-full opacity-75`}
+                  ></span>
+                  <span
+                    class={`${
+                      project.active ? "bg-green-500" : "bg-red-500"
+                    } relative inline-flex rounded-full h-3 w-3`}
+                  ></span>
+                </span> */}
+                <p className="capitalize text-sm text-customGrayLight">
+                  {project.status}
+                </p>
+              </div>
+              <div className="flex space-x-5">
+                {project.sourceCode && (
+                  <a
+                    href={project.sourceCode}
+                    className="p-3 transition duration-300 ease-in-out hover:scale-125"
+                  >
+                    <FontAwesomeIcon
+                      icon={faCode}
+                      // className="text-primary"
+                    />
+                  </a>
+                )}
+                {project.live && (
+                  <a
+                    href={project.live}
+                    className="p-3 transition duration-300 ease-in-out hover:scale-125"
+                  >
+                    <FontAwesomeIcon
+                      icon={faArrowUpRightFromSquare}
+                      // className="text-primary"
+                    />
+                  </a>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ components/Works.js
@@ -0,0 +1,143 @@
+import {
+  faArrowUpRightFromSquare,
+  faCode,
+  faCodeBranch,
+} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import Projects from "./Projects"
+
+const projects = [
+  {
+    name: "Ticketplace",
+    description:
+      "Ticketplace is a ticket decentralized exchange website. that uses Blockchain technology to solve the problem of ticket fraud.",
+    img: "/images/ticketplace.jpeg",
+    tech: [
+      "React.js",
+      "Node.js",
+      "Solidity",
+      "JavaScript",
+      "Tailwind CSS",
+      "MongoDB",
+      "Binance Smart Chain",
+    ],
+    sourceCode: "https://github.com/JerGun/ticketplace",
+    live: "https://ticketplace.me/",
+    date: "03/2022",
+    active: false,
+    status: "archive",
+    style: "object-left object-cover",
+  },
+  {
+    name: "Discord Bot",
+    description:
+      "A Discord bot built for fun. It includes a variety of features based on the builder needs, such as browsing the latest cryptocurrency prices, viewing today classes, yesterday classes or by specific day.",
+    img: "/images/discord.jpeg",
+    tech: ["Node.js", "Discord.js", "JavaScript"],
+    sourceCode: "https://github.com/JerGun/Discord-Bot",
+    live: "https://discord.com/api/oauth2/authorize?client_id=908733995326533652&permissions=431644670016&scope=bot",
+    date: "11/2021",
+    active: false,
+    status: "archive",
+  },
+  {
+    name: "Assign Me",
+    description:
+      "An application for assignments to students. You can create groups and separate channels independently. Afther that you can create assignments in individual groups and channels It also generates a summary of all scores at the end of the course.",
+    img: "/images/assignme.png",
+    tech: ["Flutter", "Dart", "Firebase", "Firestore"],
+    sourceCode: "https://github.com/JerGun/AssignMe",
+    live: "",
+    date: "10/2021",
+    active: false,
+    status: "archive",
+    style: "object-contain",
+  },
+  {
+    name: "Covid19",
+    description:
+      "An application for tracking coronavirus (Covid-19) using easysunday API to retrieve data about infected record in Thailand.",
+    img: "/images/covid19.png",
+    tech: ["Flutter", "Dart"],
+    sourceCode: "https://github.com/JerGun/Flutter-Covid19",
+    live: "",
+    date: "09/2021",
+    active: false,
+    status: "archive",
+    style: "object-contain",
+  },
+  {
+    name: "UDIX",
+    description:
+      "A web page made for coding practice with Nuxt.js and this web will have only the frontend that will be decorated using tailwind css.",
+    img: "/images/udix.jpeg",
+    tech: ["Nuxt.js", "Tailwind CSS"],
+    sourceCode: "https://github.com/JerGun/First-NuxtJS",
+    live: "https://jergun.github.io/First-NuxtJS/",
+    date: "05/2021",
+    active: false,
+    status: "archive",
+  },
+  {
+    name: "PillRem",
+    description:
+      "An application made for coding practice with Java and using SQLite as database. This application is about medication reminder and doctor appointment",
+    img: "/images/pillrem.png",
+    tech: ["Java", "SQLite"],
+    sourceCode: "https://github.com/JerGun/PillRem",
+    live: "",
+    date: "10/2020",
+    active: false,
+    status: "archive",
+    style: "object-contain",
+  },
+]
+
+export default function Works() {
+  // const [projects, setProjects] = useState([])
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    // await axios.get("/api/bio").then((res) => {
+    //   let bios = []
+    //   res.data.forEach((e) => {
+    //     bios.push(e)
+    //   })
+    //   setBios(bios)
+    // })
+    // await axios.get("/api/highlight").then((res) => {
+    //   let highlights = []
+    //   res.data.forEach((e) => {
+    //     highlights.push(e.title)
+    //     highlights.push(3000)
+    //   })
+    //   setHighlights(highlights)
+    // })
+    // setIsLoading(false)
+  }
+
+  return (
+    <div className="h-full w-full flex flex-col items-center py-20 space-y-16">
+      <p className="text-4xl font-bold capitalize">Works</p>
+      <div className="w-full space-y-20 px-32">
+        {/* <div className="h-16 w-fit space-x-3 px-10 flex items-center justify-center rounded-xl  text-black bg-white shadow-xl border border-customGrayLight">
+          <span class="relative flex h-3 w-3">
+            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+            <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+          </span>
+          <p className="text-xl font-bold">ACTIVE PROJECTS</p>
+        </div> */}
+        {/* <Projects title="Active Projects" projects={projects} active={true} /> */}
+        <Projects
+          title="Selected Projects"
+          projects={projects}
+          active={false}
+        />
+      </div>
+    </div>
+  )
+}
