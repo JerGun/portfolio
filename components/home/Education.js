bcc39bddb0d79cd6a22452ff2f35e7d16230ed20
Pawaret Muengkaew <pawaret.muengkaew@outlook.com>
2022-09-30 00:04:00 +0700

    Implement education and experience

--- components/home/Education.js
+++ components/home/Education.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import { faTrophy } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react"
+
+const educations = [
+  {
+    school: "University of Phayao",
+    archievement: "First class honours",
+    degree: "Bachelor of Engineering in Software Engineering",
+    date: "2019 - 2022",
+    project:
+      "Senior Project I : Ticketplace - Ticket decentralized exchange website using React.js, Solidity and Blockchain technology",
+    projectSecond:
+      "Senior Project II : Archery Adventure - Wave-based & third-person shooter game using Unity and C#",
+  },
+  {
+    school: "Chiang Rai Vidhayakhome School",
+    degree: "Science and Mathematics",
+    date: "2009 - 2019",
+  },
+]
 
 export default function Education() {
   return (
     <div className="h-full w-full flex flex-col items-center py-20 space-y-16">
       <p className="text-4xl font-bold capitalize">Education</p>
-      <div className="w-full flex justify-center text-4xl space-y-20 px-32">
-        Developing
+      <div className="w-7/12 flex flex-col justify-center space-y-12">
+        {educations.map((education, i) => (
+          <>
+            <div className="h-fit w-full space-y-5" key={i}>
+              <p className="text-primary">{education.date}</p>
+              {education.archievement && (
+                <div className="flex items-center space-x-3">
+                  <FontAwesomeIcon icon={faTrophy} className="text-primary" />
+                  <p className="text-xl">{education.archievement}</p>
+                </div>
+              )}
+              <p className="text-3xl font-bold">{education.degree}</p>
+              <p className="text-xl font-bold">{education.school}</p>
+              <p className="text-text">{education.project}</p>
+              <p className="text-text">{education.projectSecond}</p>
+            </div>
+            {i % 2 == 0 && (
+              <span className="h-0.5 w-full bg-white opacity-70"></span>
+            )}
+          </>
+        ))}
       </div>
     </div>
   )
--- components/home/Experiences.js
+++ components/home/Experiences.js
@@ -1,11 +1,87 @@
-import React from 'react'
+import { faCheck, faTrophy } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react"
+
+const experiences = [
+  {
+    title: "Fullstack Developer",
+    award: false,
+    archievement: "Internship",
+    description: "",
+    company: "OneDee Solution Co., Ltd.,",
+    date: "Apr 2022 - Sep 2022",
+  },
+  {
+    title: "Backend Developer",
+    award: false,
+    archievement: "Internship",
+    description: "",
+    company: "OneDee Solution Co., Ltd.,",
+    date: "Apr 2021 - May 2021",
+  },
+  {
+    title: "Entrepreneurial Ecosystem Development",
+    award: false,
+    archievement: "Attend",
+    description:
+      "Attend on behalf of the Nixing team, the topic was: Food Order Application, where each meal was allocated by a nutritionist. ",
+    company: "Innovation and Technology Transfer Institute (UPITI)",
+    date: "Mar 2021",
+  },
+  {
+    title: "Hackathon Boot Camp",
+    award: true,
+    archievement: "2nd Place Award (Second Shop)",
+    description:
+      "There are activities to enhance the thinking process in the Software Development Process.",
+    company: "20Scoops CNX Co., Ltd.,",
+    date: "Mar 2021",
+  },
+  {
+    title: "TPA'S PARTNER",
+    award: true,
+    archievement: "Top 10 finalists",
+    description:
+      "TPA's Partner is a project to find students to work with the Thai Programming Association in organizing various activities for students.",
+    company: "Thai Programming Association",
+    date: "Jan 2021",
+  },
+]
 
 export default function Experiences() {
   return (
-    <div className="h-full w-full flex flex-col items-center py-20 space-y-16">
+    <div className="h-full w-full flex flex-col items-center py-20 px-32 space-y-16">
       <p className="text-4xl font-bold capitalize">Experiences</p>
-      <div className="w-full flex justify-center text-4xl space-y-20 px-32">
-        Developing
+      <div className="w-full">
+        {experiences.map((experience, i) => (
+          <div className="h-fit w-full flex" key={i}>
+            <div className="h-full w-5/12 space-y-3 px-10">
+              <p className="text-xl font-bold">{experience.company}</p>
+              <p className="text-text">{experience.date}</p>
+            </div>
+            <div className="h-full w-7/12 flex">
+              <div className="h-full w-fit">
+                <div className="relative h-10 w-10 flex flex-col items-center justify-center">
+                  <span className="absolute h-10 w-10 rounded-full border-2 border-dashed"></span>
+                  <span className="h-5 w-5 rounded-full bg-primary"></span>
+                </div>
+                <div className="h-full w-10 flex items-center justify-center">
+                  <div className="h-40 w-0 border-[0.1px] border-dashed"></div>
+                </div>
+              </div>
+              <div className="w-11/12 px-10 py-1.5 space-y-3">
+                <p className="text-xl font-bold">{experience.title}</p>
+                {experience.archievement && (
+                  <div className="flex items-center space-x-3">
+                    <FontAwesomeIcon icon={experience.award ? faTrophy : faCheck} className="text-primary" />
+                    <p>{experience.archievement}</p>
+                  </div>
+                )}
+                <p className="text-text">{experience.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
