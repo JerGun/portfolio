77906edbf62469ac1b1e8f8df1f3d9f0350a94fc
Pawaret Muengkaew <pawaret.muengkaew@outlook.com>
2022-10-07 22:49:18 +0700

    Add award images

--- components/home/Awards.js
+++ components/home/Awards.js
@@ -5,12 +5,14 @@ import {
   faTrophy,
 } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import React from "react"
+import React, { useState } from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Autoplay, Keyboard } from "swiper"
+import Lightbox from "react-awesome-lightbox"
 
 import "swiper/css"
 import "swiper/css/scrollbar"
+import "react-awesome-lightbox/build/style.css"
 
 const awards = [
   {
@@ -30,6 +32,16 @@ const awards = [
     date: "2019, 2021",
     type: "",
     icon: faAward,
+    images: [
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/pawaret-portfolio.appspot.com/o/Hackathon.png?alt=media&token=f41f3ea6-57ae-4cbd-a5f5-e112513ca3ca",
+        title: "Outstanding Academic Performance Year 2019",
+      },
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/pawaret-portfolio.appspot.com/o/Hackathon.png?alt=media&token=f41f3ea6-57ae-4cbd-a5f5-e112513ca3ca",
+        title: "Outstanding Academic Performance Year 2021",
+      },
+    ],
   },
   {
     name: "Good Academic Performance",
@@ -39,6 +51,16 @@ const awards = [
     date: "2020 - 2021",
     type: "Academic Achievement Awards",
     icon: faAward,
+    images: [
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/pawaret-portfolio.appspot.com/o/Hackathon.png?alt=media&token=f41f3ea6-57ae-4cbd-a5f5-e112513ca3ca",
+        title: "Good Academic Performance Year 2020",
+      },
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/pawaret-portfolio.appspot.com/o/Hackathon.png?alt=media&token=f41f3ea6-57ae-4cbd-a5f5-e112513ca3ca",
+        title: "Good Academic Performance Year 2021",
+      },
+    ],
   },
   {
     name: "Hackathon Boot Camp",
@@ -47,12 +69,46 @@ const awards = [
     date: "2021",
     type: "",
     icon: faTrophy,
+    images: [
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/pawaret-portfolio.appspot.com/o/Hackathon.png?alt=media&token=f41f3ea6-57ae-4cbd-a5f5-e112513ca3ca",
+        title: "Hackathon Boot Camp",
+      },
+    ],
   },
 ]
 
 export default function Awards() {
+  const [imageIndex, setImageIndex] = useState(null)
+
   return (
-    <div className="h-full w-full flex flex-col items-center py-20 space-y-16">
+    <div className="relativeh-full w-full flex flex-col items-center py-20 space-y-16">
+      <div className="absolute h-full w-full">
+        {awards.map((award, i) =>
+          imageIndex == i ? (
+            award.images?.length > 1 ? (
+              <Lightbox
+                images={award.images}
+                onClose={() => {
+                  setImageIndex(null)
+                }}
+                key={i}
+              />
+            ) : (
+              award.images.map((image, j) => (
+                <Lightbox
+                  title={image.title}
+                  image={image.url}
+                  onClose={() => {
+                    setImageIndex(null)
+                  }}
+                  key={j}
+                />
+              ))
+            )
+          ) : null
+        )}
+      </div>
       <p className="text-4xl font-bold capitalize">Awards</p>
       <div className="h-full w-10/12 flex justify-center items-center">
         <Swiper
@@ -74,7 +130,7 @@ export default function Awards() {
         >
           {awards.map((award, i) => (
             <SwiperSlide key={i}>
-              <div className="relative h-[350px] w-full flex items-end">
+              <div className="relative h-[375px] w-full flex items-end">
                 <div className="absolute h-12 w-12 top-0 left-1/2 transform -translate-x-1/2 flex items-center justify-center rounded-full shadow-md bg-primary">
                   <FontAwesomeIcon
                     icon={award.icon}
@@ -82,19 +138,30 @@ export default function Awards() {
                     size="lg"
                   />
                 </div>
-                <div className="h-[325px] w-full p-5 pt-10 space-y-5 text-center rounded-xl bg-customGrayHeavy">
-                  <div className="h-[40%] flex flex-col items-center justify-between space-y-1">
+                <div className="h-[350px] w-full p-5 pt-10 space-y-5 text-center rounded-xl bg-customGrayHeavy">
+                  <div className="h-[50%] flex flex-col items-center space-y-3">
                     <p className="text-xl font-bold">{award.name}</p>
                     <p>{award.organization}</p>
                     <p className="text-primary">{award.date}</p>
+                    {award.images && (
+                      <div className="w-full flex justify-center">
+                        <button
+                          onClick={() => {
+                            setImageIndex(i)
+                          }}
+                          className=""
+                        >
+                          <FontAwesomeIcon
+                            icon={faImage}
+                            className=""
+                            size="lg"
+                          />
+                        </button>
+                      </div>
+                    )}
                   </div>
                   <div className="h-0.5 w-full bg-customGrayLight opacity-50"></div>
                   <p>{award.description}</p>
-                  <div className="w-full flex justify-center">
-                    <button onClick={() => {}}>
-                      <FontAwesomeIcon icon={faImage} className="" size="lg" />
-                    </button>
-                  </div>
                 </div>
               </div>
             </SwiperSlide>
