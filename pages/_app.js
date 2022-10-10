6d13a5758fde5e6dde7acb861c1db62f0b9cf7e7
Pawaret Muengkaew <pawaret.muengkaew@outlook.com>
2022-10-10 22:50:30 +0700

    Improve smooth scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,16 @@
 import "../styles/globals.css"
 import "@fortawesome/fontawesome-svg-core/styles.css" // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core"
-import Footer from "@components/Footer"
-import { useRouter } from "next/router"
-import { useEffect } from "react"
-// import SmoothScroll from "@components/SmoothScroll"
+import SmoothScroll from "@components/SmoothScroll"
 
 config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
   return (
-    <div className="relative h-full w-full bg-white select-none">
-      {/* <SmoothScroll /> */}
-      <Component {...pageProps} />
-      {/* {!router.pathname.startsWith("/admin")  && <Footer />} */}
+    <div className="h-full w-full bg-white select-none">
+      {/* <SmoothScroll> */}
+        <Component {...pageProps} />
+      {/* </SmoothScroll> */}
     </div>
   )
 }
