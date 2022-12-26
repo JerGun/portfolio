4e34eaa8d0ca11b1f0a64e49d5742148c2a31ad7
Pawaret Muengkaew <pawaret.muengkaew@outlook.com>
2022-12-26 12:41:45 +0700

    add footer dynamic copyright year

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCopyright } from "@fortawesome/free-regular-svg-icons"
 
 export default function Footer() {
+  const formatYear = () => {
+    const startYear = 2022
+    const currentYear = new Date().getFullYear()
+    if (startYear != currentYear) return `${startYear} - ${currentYear}`
+    return startYear
+  }
+
   return (
     <div className="h-fit w-full flex justify-center items-center flex-wrap space-x-1 p-3 text-xs md:text-sm text-customGrayLight bg-background opacity-90">
       <FontAwesomeIcon icon={faCopyright} />
-      <p>All rights are reserved 2022. Powered by </p>
+      <p>{`All rights are reserved ${formatYear()}. Powered by`} </p>
       <a
         href="https://facebook.com/GongPawaret/"
         target="_blank"
